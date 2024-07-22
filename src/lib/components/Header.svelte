5b8e5b2155450e070f1611cde3e4d4c4b2ed6702
Vladimir Guerrero <vladimirguerrero22@gmail.com>
2024-07-22 16:47:04 +0800

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { PrismicLink } from '@prismicio/svelte';
+	import WordMark from './WordMark.svelte';
+	import ButtonLink from './ButtonLink.svelte';
+
+	/** @type {import("@prismicio/client").Content.SettingsDocument}*/
+
+	export let settings;
+</script>
+
+<header>
+	<nav
+		class="flex flex-col items-center justify-between gap-6 border-t border-gray-600 px-8 py-7 md:flex-row"
+		aria-label="Main"
+	>
+		<a href="/">
+			<WordMark />
+			<span class="sr-only">{settings.data.site_title} home page</span>
+		</a>
+
+		<ul class="flex gap-6">
+			{#each settings.data.navigation as item (item.label)}
+				<li>
+					{#if item.cta_button}
+						<ButtonLink field={item.link}>
+							{item.label}
+						</ButtonLink>
+					{:else}
+						<PrismicLink field={item.link} class="inline-flex min-h-11 items-center">
+							{item.label}
+						</PrismicLink>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
---
+++ src/lib/slices/Bento/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Bounded from '$lib/components/Bounded.svelte';
+	import type { Content } from '@prismicio/client';
+	import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
+	import GoldText from './GoldText.svelte';
+	import Heading2 from '$lib/components/Heading2.svelte';
+
+	export let slice: Content.BentoSlice;
+</script>
+
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<PrismicRichText
+		field={slice.primary.heading}
+		components={{ em: GoldText, heading2: Heading2 }}
+	/>
+	<PrismicRichText field={slice.primary.body} />
+
+	{#each slice.primary.repeatable_zone as item}
+		<!-- Render content for item -->
+		<PrismicRichText field={item.title} />
+		<PrismicRichText field={item.body} />
+		<PrismicImage field={item.image} />
+	{/each}
+</Bounded>
