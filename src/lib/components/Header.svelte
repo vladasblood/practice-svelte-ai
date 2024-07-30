381ffff5aa0d7fa7f32630e759d3624e57cecaff
Vladimir Guerrero <vladimirguerrero22@gmail.com>
2024-07-30 09:22:40 +0800

    header-update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,6 +6,8 @@
 
 	import IconClose from '~icons/ph/x-bold';
 	import IconMenu from '~icons/ph/list-bold';
+	import { asLink } from '@prismicio/client';
+	import { page } from '$app/stores';
 
 	/** @type {import("@prismicio/client").Content.SettingsDocument}*/
 
@@ -14,44 +16,66 @@
 	let isOpen = true;
 	const toggleOpen = () => (isOpen = !isOpen);
 	const close = () => (isOpen = false);
+
+	/** @param {import('@prismicio/client').LinkField} link */
+	const isActive = (link) => {
+		const path = asLink(link);
+
+		return path && $page.url.pathname.includes(path);
+	};
 </script>
 
 <header class="p-4 md:p-6">
 	<nav
-		class="flex-max-w-6xl font mx-auto flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center"
+		class="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center"
 		aria-label="Main"
 	>
 		<div class="flex items-center justify-between">
-			<a href="/">
+			<a href="/" on:click={close} class="z-50">
 				<WordMark />
 				<span class="sr-only">{settings.data.site_title} home page</span>
 			</a>
 
-			<button type="button" class="block p-2 text-3xl text-white md:hidden" on:click={toggleOpen}
-				><IconMenu /></button
+			<button
+				aria-expanded={isOpen}
+				type="button"
+				class="block p-2 text-3xl text-white md:hidden"
+				on:click={toggleOpen}><IconMenu /></button
 			>
 		</div>
 
 		<!-- Mobile Nav -->
 		<div
 			class={clsx(
-				'tranistion-transform fixed inset-0 z-40 flex flex-col items-end bg-gray-950 pr-4 pt-14 duration-300 ease-in-out md:hidden',
+				'fixed inset-0 z-40 flex flex-col items-end bg-gray-950 pr-4 pt-14 transition-transform duration-300 ease-in-out md:hidden',
 				isOpen ? 'translate-x-0' : 'translate-x-[100%]'
 			)}
 		>
-			<button type="button" class="block p-2 text-3xl text-white md:hidden" on:click={toggleOpen}
-				><IconClose /></button
+			<button
+				aria-expanded={isOpen}
+				type="button"
+				class="block p-2 text-3xl text-white md:hidden"
+				on:click={toggleOpen}><IconClose /></button
 			>
 
 			<ul class="grid justify-items-end gap-8">
 				{#each settings.data.navigation as item (item.label)}
 					<li>
 						{#if item.cta_button}
-							<ButtonLink field={item.link}>
+							<ButtonLink
+								aria-current={isActive(item.link) ? 'page' : undefined}
+								field={item.link}
+								on:click={close}
+							>
 								{item.label}
 							</ButtonLink>
 						{:else}
-							<PrismicLink field={item.link} class="block min-h-11 px-3 text-3xl first:mt-8">
+							<PrismicLink
+								on:click={close}
+								field={item.link}
+								class="block min-h-11 px-3 text-3xl first:mt-8"
+								aria-current={isActive(item.link) ? 'page' : undefined}
+							>
 								{item.label}
 							</PrismicLink>
 						{/if}
@@ -65,11 +89,20 @@
 			{#each settings.data.navigation as item (item.label)}
 				<li>
 					{#if item.cta_button}
-						<ButtonLink field={item.link}>
+						<ButtonLink
+							field={item.link}
+							on:click={close}
+							aria-current={isActive(item.link) ? 'page' : undefined}
+						>
 							{item.label}
 						</ButtonLink>
 					{:else}
-						<PrismicLink field={item.link} class="inline-flex min-h-11 items-center">
+						<PrismicLink
+							on:click={close}
+							field={item.link}
+							class="inline-flex min-h-11 items-center"
+							aria-current={isActive(item.link) ? 'page' : undefined}
+						>
 							{item.label}
 						</PrismicLink>
 					{/if}
